73d2562ba30784f7224c772947b0bf37e0d75848
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-12-02 00:20:00 +0530

    some updates

--- src/components/continue/Info.jsx
+++ src/components/continue/Info.jsx
@@ -8,59 +8,77 @@ import DropIcon from "../../assets/dropicon.svg";
 const Info = () => {
   return (
     <>
-      <div id="NotificationspaceRoot" className="bg-[#fead3a] w-full h-8" />
-      {/* <div className="max-h-[26.3rem]">
-        <div className=" absolute top-8">
-          <img
-            src={BackgroundImg}
-            alt="background/img"
-            className="md:w-[720px] md:h-[720px] float-right"
-          />
-          <div className="bg-gradient-to-b from-transparent to-white h-44 flex  justify-center items-center flex-col mt-auto absolute top-64 w-full txt ">
-            <h3 className="text-center text-blue-950 text-6xl font-nunito uppercase font-extrabold p-3">
+      <div
+        id="NotificationspaceRoot"
+        className="bg-[#fead3a] w-full h-8 md:h-0"
+      />
+      <div>
+        {/* background image section starts */}
+        <div className="md:max-w-[40%] md:h-[720px] lg:h-[100vh] h-[390px] flex-shrink-0 flex md:float-right">
+          <img src={BackgroundImg} alt="background/img" className="w-[100%]" />
+        </div>
+        {/* background image section ends */}
+        {/* login content sections starts */}
+        <div className="md:bg-[#0F002B] md:min-w-[60%] lg:h-[100vh] md:h-[720px] flex flex-col md:float-left">
+          {/* main and sub heading starts */}
+          <div className="justify-center items-center md:mt-[88px] bg-gradient md:bg-none flex md:w-full  -mt-[15.5rem] || bg-cover bg-50%_50% bg-blend-normal bg-no-repeat relative  flex-col  w-full">
+            <h3 className="text-center md:text-white  text-5xl font-['Nunito_Sans'] uppercase font-extrabold p-3 w-[337px] text-[#0f002b]">
               Ludo King
             </h3>
-            <h5 className="font-oooh text-4xl font-bold text-center text-blue-950">
+            <h5 className="font-oooh text-4xl font-bold text-center md:text-white">
               Punch Line
             </h5>
           </div>
-        </div>
-      </div> */}
-      <div className="bg-[linear-gradient(180deg,_rgba(255,_255,_255,_0)_0%,#ffffff_100%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat relative flex flex-row justify-center w-full items-start">
-        <img src={BackgroundImg} alt="background/img" className="w-8" />
-        <div className="text-center text-6xl font-['Nunito_Sans'] font-bold text-[#0f002b] absolute top-16 left-6 h-20 w-[337px]">
-          LUDO KING
-        </div>
-        <div className="text-center text-4xl font-['Oooh_Baby'] text-[#0f002b] relative mt-32">
-          punch line
-        </div>
-      </div>
-      <div className="overflow-hidden bg-white flex flex-col justify-end pt-3 gap-8 w-full items-start">
-        <div className="relative flex flex-row justify-center ml-4 w-full items-start">
-          <div
-            id="Line"
-            className="border-solid w-full h-px absolute top-6 left-0 border-t border-b-0 border-black/30 border-x-0"
-          />
-          <div className="text-center text-xl font-['Inter'] font-bold text-[#0f002b] bg-white relative flex flex-row justify-center pt-4 w-3/5 h-12 items-start rounded-lg">
-            Log in or sign up
-          </div>
-        </div>
-        <div className="flex flex-col mb-0 gap-6 w-full items-start">
-          <div className="flex w-[325px] h-[56px] px-[16px] py-[10px] justify-center items-center gap-[10px] border-4  mx-auto mt-14 rounded-[10px]">
-            <div>
-              <img src={CountryIcon} />
+          {/* main and sub heading ends */}
+          {/* content section start  */}
+          <div className="flex flex-col gap-16 w-full items-start mt-[12px] md:mt-[76px]">
+            {/* signup tags starts  */}
+            <div className="relative flex flex-row justify-center w-full md:items-start">
+              <div
+                id="Line"
+                className="border-solid border-black/30 md:border-white/30 w-full h-px absolute top-6 left-0 border-t border-b-0 border-x-0"
+              />
+              <div className="text-center text-lg font-['Inter'] font-medium text-[#0f002b] md:bg-[#fead3a] relative flex flex-row justify-center pt-3 w-1/2 h-12 items-start rounded-lg bg-white">
+                Log in or sign up
+              </div>
             </div>
-            <div>
-              <img src={DropIcon} />
+            {/* signup tags ends  */}
+            {/* number and continue-btn section  */}
+            <div className="flex flex-col ml-1 gap-6 w-full items-start">
+              <div
+                id="EnterPhoneNumber1"
+                className="shadow-[0px_0px_4px_0px_rgba(0,_0,_0,_0.25)] items-center mx-auto bg-white flex flex-row gap-4 justify-center pt-3 px-4 rounded-lg w-[325px] h-[56px] py-[10px]"
+              >
+                <div className="flex flex-row gap-2 w-16 items-start">
+                  <img
+                    src={CountryIcon}
+                    alt="Twemojiflagindia"
+                    id="Twemojiflagindia"
+                    className="w-8"
+                  />
+                  <img
+                    src={DropIcon}
+                    alt="Gridiconsdropdown"
+                    id="Gridiconsdropdown"
+                    className="mt-1 w-5"
+                  />
+                </div>
+                <div className="text-xl font-['Inter'] font-medium text-[rgba(15,_0,_43,_0.3)] mt-1">
+                  Enter Phone Number
+                </div>
+              </div>
+              <div
+                id="ContinueAndLoading"
+                className="text-center items-center text-xl mx-auto font-['Inter'] h-[56px] w-[325px] px-[16px] text-[20px] font-bold md:py-[0px] gap-[10px]  md:text-[#0f002b] shadow-[0px_0px_4px_0px_rgba(0,_0,_0,_0.25)] md:bg-white flex flex-row justify-center pt-4 rounded-lg text-white bg-[#0f002b] p-4"
+              >
+                Continue
+              </div>
             </div>
-            <div>
-              <input type="number" placeholder="Enter Phone Number" />
-            </div>
+            {/* number and continue-btn section  */}
           </div>
-          <div className="inline-flex h-[56px] w-[325px] px-[16px] py-[10px] gap-[10px] items-center justify-center rounded-[10px] mt-8 font-inter leading-normal text-center text-[20px] font-['Inter'] font-bold text-white shadow-[0px_0px_4px_0px_rgba(0,_0,_0,_0.25)] bg-[#0f002b]  flex-row  mb-px ml-8 pt-4">
-            <button className="">Continue</button>
-          </div>
+          {/* content section end */}
         </div>
+        {/* login content sections ends */}
       </div>
     </>
   );
