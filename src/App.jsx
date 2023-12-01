937dc9b0bab41dbba8eb2bca0dc5554175612ba8
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-12-02 00:19:33 +0530

    updated preferences

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import SignUpDesktop from "./components/app_start/SignUpDesktop";
 import Info from "./components/continue/Info";
+import NewGame from "./components/newgane/NewGame";
+import ResponGame from "./components/newgane/ResponGame";
 
 function App() {
   return (
     <div>
       {/* <Info /> */}
-      <SignUpDesktop />
+      {/* <SignUpDesktop /> */}
+      {/* <Trial /> */}
+      {/* <NewGame /> */}
+      <ResponGame />
     </div>
   );
 }
