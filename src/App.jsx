15b7ed834469e2ded6f4a671026570b946462ad5
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-12-04 21:59:04 +0530

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import Info from "./components/continue/Info";
 import LiveBattle from "./components/live_battle/LiveBattle";
 import NewGame from "./components/newgane/NewGame";
 import ResponGame from "./components/newgane/ResponGame";
+import NewProfile from "./components/profile/NewProfile";
+import NewProfileMob from "./components/profile/NewProfileMob";
+import NewProfileResp from "./components/profile/NewProfileResp";
 
 function App() {
   return (
     <div>
       {/* <Info /> */}
       {/* <SignUpDesktop /> */}
-      {/* <Trial /> */}
       {/* <NewGame /> */}
       {/* <ResponGame /> */}
-      <LiveBattle />
+      {/* <LiveBattle /> */}
+      {/* <NewProfile /> */}
+      {/* <NewProfileMob /> */}
+      <NewProfileResp />
     </div>
   );
 }
