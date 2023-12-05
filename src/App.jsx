77238882dec6f493526ca965cae13525385479fe
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-12-05 20:24:02 +0530

    rendering components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import "./App.css";
 import SignUpDesktop from "./components/app_start/SignUpDesktop";
 import Info from "./components/continue/Info";
+import FilterLiveBattle from "./components/live_battle/FilterLiveBattle";
 import LiveBattle from "./components/live_battle/LiveBattle";
-import NewGame from "./components/newgane/NewGame";
-import ResponGame from "./components/newgane/ResponGame";
+import LiveBattle2 from "./components/live_battle/LiveBattle2";
+import OpenChallengeReq from "./components/live_battle/OpenChallengeReq";
+import NewGame from "./components/live_battle/NewGame";
+import ResponGame from "./components/live_battle/ResponGame";
 import NewProfile from "./components/profile/NewProfile";
 import NewProfileMob from "./components/profile/NewProfileMob";
 import NewProfileResp from "./components/profile/NewProfileResp";
+import GameCash from "./components/listedgame/GameCash";
+import CardClickInstance from "./components/listedgame/CardClickInstance";
+import CardClickInstance2 from "./components/listedgame/CardClickInstance2";
+import CardClickInstance3 from "./components/listedgame/CardClickInstance3";
+import InstanceStop from "./components/listedgame/InstanceStop";
+import RequestMatch from "./components/listedgame/RequestMatch";
+import RequestCounter from "./components/listedgame/RequestCounter";
+import MatchStart from "./components/listedgame/MatchStart";
+import NewGameMob from "./components/live_battle/NewGameMob";
 
 function App() {
   return (
@@ -15,10 +27,22 @@ function App() {
       {/* <SignUpDesktop /> */}
       {/* <NewGame /> */}
       {/* <ResponGame /> */}
+      {/* <NewGameMob /> */}
       {/* <LiveBattle /> */}
+      {/* <LiveBattle2 /> */}
+      {/* <FilterLiveBattle /> */}
+      {/* <OpenChallengeReq /> */}
+      {/* <GameCash /> */}
+      {/* <CardClickInstance /> */}
+      {/* <CardClickInstance2 /> */}
+      {/* <CardClickInstance3 /> */}
+      {/* <InstanceStop /> */}
+      {/* <RequestMatch /> */}
+      {/* <RequestCounter /> */}
+      {/* <MatchStart /> */}
       {/* <NewProfile /> */}
-      {/* <NewProfileMob /> */}
-      <NewProfileResp />
+      <NewProfileMob />
+      {/* <NewProfileResp /> */}
     </div>
   );
 }
