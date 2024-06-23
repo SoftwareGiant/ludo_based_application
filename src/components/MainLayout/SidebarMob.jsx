4202a54ba8a8188a686fd4389bd43c1878921906
Balajee Mishra <balajee.84068@gmail.com>
2024-06-23 23:19:09 +0530

    merged the changes

--- src/components/MainLayout/SidebarMob.jsx
+++ src/components/MainLayout/SidebarMob.jsx
@@ -10,7 +10,6 @@ import Profile from "../../assets/new_game/profile.svg";
 import ReferEarn from "../../assets/new_game/refer&earn.svg";
 import Support from "../../assets/new_game/support.svg";
 import BackBlack from "../../assets/profile/back_black.svg";
-import { IoMenu } from "react-icons/io5";
 import { Icon } from "@iconify-icon/react";
 
 export function SidebarMob({
